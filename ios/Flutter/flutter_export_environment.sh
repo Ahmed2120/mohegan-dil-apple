c3fe37513d2352af4b5fb3a89143bd2ac00f79af
Ahmed2120 <ahmed202ashraf202@gmail.com>
2024-01-23 00:44:23 +0200

    last

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\flutter"
-export "FLUTTER_APPLICATION_PATH=E:\tqnia\mohegan-Delivery-github"
+export "FLUTTER_APPLICATION_PATH=E:\tqnia\mohegan-Delivery"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
